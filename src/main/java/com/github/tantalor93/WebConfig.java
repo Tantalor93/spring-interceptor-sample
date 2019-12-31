ef3aa410ef8c46948254377b0eb7ce4ee1b70b1b
Ondrej Benkovsky <ondrej.benkovsky@wandera.com>
2019-12-31 15:19:12 +0100

    interceptors

--- src/main/java/com/github/tantalor93/WebConfig.java
+++ src/main/java/com/github/tantalor93/WebConfig.java
@@ -1,16 +1,29 @@
 package com.github.tantalor93;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+
+	@Autowired
+	private BenkyInterceptor benkyInterceptor;
 
 	@Bean
 	public FilterRegistrationBean filterRegistrationBean() {
 		FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new Sample3Filter());
 		filterRegistrationBean.addUrlPatterns("/benky");
 		return filterRegistrationBean;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		InterceptorRegistration interceptorRegistration = registry.addInterceptor(benkyInterceptor);
+		interceptorRegistration.addPathPatterns("/sample");
 	}
 }
